4cbe6e0567ca310521329ae698b8988d861a00c9
McB0y <rafael.cisneros.gomez@gmail.com>
2017-10-23 23:57:00 -0500

    Proxies are a very hot topic included within this mini project.

---
+++ weather-data.js
@@ -0,0 +1,19 @@
+// My constructor by default
+export class WeatherData {
+    constructor(cityName, description) {
+        this.cityName = cityName;
+        this.description = description;
+        this.temperature = '';
+    }
+}
+
+// Hot feature 'proxies', you can grab the temperature and transform it likewise
+export const WEATHER_PROXY_HANDLER = {
+    get: function(target, property) {
+        return Reflect.get(target, property);
+    },
+    set: function(target, property, value) {
+        const newValue = (value * 1.8 + 32).toFixed(2) + 'F.';
+        return Reflect.set(target, property, newValue);
+    }
+};
